522e7e8666861e1d5c1ffde8bd32c8f60072cd67
praveenK257 <apraveenkumar25701@gmail.com>
2022-09-18 18:35:49 +0530

    react project initialization

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
